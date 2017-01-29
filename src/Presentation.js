7b868f45bf9509ddb14e200a34929d84d250d95a
Gordon <geniusgordon@gmail.com>
2017-01-29 21:23:37 +0800

    upgrade spectacle to 2.0

--- src/Presentation.js
+++ src/Presentation.js
@@ -1,8 +1,34 @@
 import React from 'react';
 /* eslint-disable no-unused-vars */
 import {
-  Appear, BlockQuote, Cite, CodePane, Code, Deck, Fill, Fit,
-  Heading, Image, Layout, ListItem, List, Quote, Spectacle, Slide, Text,
+  Appear,
+  BlockQuote,
+  Cite,
+  Code,
+  CodePane,
+  ComponentPlayground,
+  Deck,
+  Fill,
+  Fit,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  List,
+  ListItem,
+  Markdown,
+  MarkdownSlides,
+  Quote,
+  S,
+  Slide,
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderItem,
+  TableItem,
+  TableRow,
+  Text,
+  Typeface,
 } from 'spectacle';
 /* eslint-enable no-unused-vars */
 import createTheme from "spectacle/lib/themes/default";
@@ -14,17 +40,18 @@ const theme = createTheme({}, {
 });
 
 const Presentation = () => (
-  <Spectacle theme={theme}>
-    <Deck transition={['slide']}>
-      <Slide>
-        <Heading fit>Spectacle Boilerplate</Heading>
-        <Text>By geniusgordon</Text>
-      </Slide>
-      <Slide>
-        <Text>To get started, edit <Code>src/Presentation.js</Code> and save to reload.</Text>
-      </Slide>
-    </Deck>
-  </Spectacle>
+  <Deck theme={theme} transition={['slide']}>
+    <Slide>
+      <Heading fit>Spectacle Boilerplate</Heading>
+      <Text>By geniusgordon</Text>
+    </Slide>
+    <Slide>
+      <Text>To get started, edit <Code>src/Presentation.js</Code> and save to reload.</Text>
+    </Slide>
+    <Slide>
+      <ComponentPlayground />
+    </Slide>
+  </Deck>
 );
 
 export default Presentation;
